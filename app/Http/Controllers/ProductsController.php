db764bba501283ebf9ee64cf9351a6cd398238fe
yinka1255 <adeniranadeyinka101@gmail.com>
2021-07-24 17:35:59 +0100

    Update

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -98,7 +98,7 @@ class ProductsController extends Controller{
         $product = Product::where("products.id", $product_id)->first();
         $similar_products = Product::where("category_id", $product->category_id)
                             ->where("id", "!=", $product->id)
-                            ->take(8)->get();
+                            ->take(15)->get();
         return view('customers/details')->with(["product"=>$product, "similar_products"=>$similar_products]);
     }  
 
